aede476ebdf97c103bf3a0c4b8c8b415ed8c83e6
SheldonZheng <zhenghangtxdyr@gmail.com>
2022-01-25 14:17:46 +0800

    初始化服务注册器

--- baiye-rpc-server/src/main/java/space/baiye/rpc/server/service/ServiceStarter.java
+++ baiye-rpc-server/src/main/java/space/baiye/rpc/server/service/ServiceStarter.java
@@ -8,6 +8,8 @@ import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
 import lombok.extern.slf4j.Slf4j;
 import space.baiye.rpc.common.annotation.RpcService;
 import space.baiye.rpc.common.codec.MessageDecoder;
@@ -57,6 +59,7 @@ public class ServiceStarter {
         this.zookeeperAddress = this.properties.getProperty(Config.ZOOKEEPER_URL, "");
         this.serverIP = this.properties.getProperty(Config.SERVER_IP,"127.0.0.1");
         this.serverPort = Integer.valueOf(this.properties.getProperty(Config.SERVER_PORT,"8891"));
+        this.serviceRegister = new ServiceRegister();
         log.info("properties loaded. zkAddress : {}, serverIP : {}, serverPort : {}",zookeeperAddress,serverIP,serverPort);
         scanBean();
         log.info("scan bean over. size : {}",serviceContainer.size());
@@ -78,19 +81,24 @@ public class ServiceStarter {
             ServerBootstrap bootstrap = new ServerBootstrap();
             bootstrap.group(boosGroup,workerGroup)
                     .channel(NioServerSocketChannel.class)
-                    .option(ChannelOption.SO_BACKLOG,1024)
+                    // TCP默认开启了 Nagle 算法，该算法的作用是尽可能的发送大数据快，减少网络传输。TCP_NODELAY 参数的作用就是控制是否启用 Nagle 算法。
+                    .childOption(ChannelOption.TCP_NODELAY,true)
+                    // 是否开启 TCP 底层心跳机制
+                    .childOption(ChannelOption.SO_KEEPALIVE,true)
+                    //表示系统用于临时存放已完成三次握手的请求的队列的最大长度,如果连接建立频繁，服务器处理创建新连接较慢，可以适当调大这个参数
+                    .option(ChannelOption.SO_BACKLOG,512)
+                    .handler(new LoggingHandler(LogLevel.INFO))
                     .childHandler(new ChannelInitializer<SocketChannel>() {
                         @Override
                         protected void initChannel(SocketChannel socketChannel) throws Exception {
-                            socketChannel.pipeline().addLast(new MessageDecoder(RpcReq.class));
-
-                            socketChannel.pipeline().addLast(new MessageEncoder());
-
-                            socketChannel.pipeline().addLast(new ServiceNettyHandler(serviceContainer));
+                            socketChannel.pipeline()
+                                    .addLast("decoder",new MessageDecoder(RpcReq.class))
+                                    .addLast("encoder",new MessageEncoder())
+                                    .addLast("handler",new ServiceNettyHandler(serviceContainer));
                         }
                     });
 
-            ChannelFuture fu = bootstrap.bind(serverIP, serverPort).sync();
+            ChannelFuture fu = bootstrap.bind(serverPort).sync();
             log.info("bind port success . start listen");
 
             registerServiceToZk();
